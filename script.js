0049dec106119ad32a83a492d63d17c1a88a38e5
TaHasib981 <tahaisb981@gmail.com>
2022-02-17 21:30:28 +0600

    error handling done

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-document.getElementById('calculate-btn').addEventListener('click',function(){
-    const foodInput = document.getElementById('food-input').value
-    const rentInput = document.getElementById('rent-input').value
-    const clothesInput = document.getElementById('clothes-input').value
 
-    const allExpenses = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothesInput)
+document.getElementById('calculate-btn').addEventListener('click',function(){
+   
+    const income = document.getElementById('income-input').value
+    const incomeFloat = parseFloat(income)
+    const food = document.getElementById('food-input').value
+    const rent = document.getElementById('rent-input').value
+    const clothes = document.getElementById('clothes-input').value 
+
+    // const myIncomefloat = parseFloat(myIncomeValue)
+ 
+
+    if(income < 0 || food < 0 || rent < 0 || clothes < 0 ){
+        alert('please ensure the number possitive that you entered !!')
+    
+    }
+    else{
+        const foodInput = document.getElementById('food-input').value
+        const rentInput = document.getElementById('rent-input').value
+        const clothesInput = document.getElementById('clothes-input').value
+
+        const allExpenses = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothesInput)
     
     
     // totalExpenses
@@ -12,12 +28,12 @@ document.getElementById('calculate-btn').addEventListener('click',function(){
     totalExpenses.innerText = allExpenses
  
   
-//    income
-    const myIncome = document.getElementById('income-input').value
-    const myIncomeFloat = parseFloat(myIncome)
-    const totalIncome = myIncomeFloat
-   
-   
+
+   //    income
+   const myIncome = document.getElementById('income-input').value
+   const myIncomeFloat = parseFloat(myIncome)
+   const totalIncome = myIncomeFloat
+  
 
    // Balance
 
@@ -26,13 +42,39 @@ document.getElementById('calculate-btn').addEventListener('click',function(){
    balance.innerText = totalIncome - parseFloat( totalExpenses.innerText)
    
  
+    }
+
+
+    
 })
 
 // save section
 
 document.getElementById('save-button').addEventListener('click',function(){
-   
+//    error function document----------------------
+    const myIncome = document.getElementById('income-input').value
+    const myIncomeFloat = parseFloat(myIncome)
+    const totalIncome = myIncomeFloat
+
     const saveInput = document.getElementById('save-input').value 
+    const saveInputFloat = parseFloat(saveInput)
+    
+    const savingAmmount = document.getElementById('saving-ammout')
+     const savingAmmountText = savingAmmount.innerText
+     const savingAmmountFloat = parseFloat(savingAmmountText)
+     savingAmmount.innerText = (totalIncome/100)*saveInputFloat
+
+     const previousBalance = document.getElementById('balance')
+    const previousBalanceText =previousBalance.innerText
+    const previousBalanceFloat = parseFloat(previousBalanceText)
+    
+    // error fucntion document end------------------------
+    
+    if(previousBalanceFloat < savingAmmount.innerText){
+        alert('You have not sufficient balance to save')
+    }
+    else{   
+        const saveInput = document.getElementById('save-input').value 
     const saveInputFloat = parseFloat(saveInput)
  
  
@@ -55,4 +97,5 @@ document.getElementById('save-button').addEventListener('click',function(){
    const remainingAmmountText = remainingAmmount.innerText
    const finalAmmount = previousBalanceFloat - parseFloat( savingAmmount.innerText)
    remainingAmmount.innerText = finalAmmount
+    }
  })
