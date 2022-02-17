5a0701464e72ec6ef7629d5d8f080a3941793a66
TaHasib981 <tahaisb981@gmail.com>
2022-02-17 15:48:40 +0600

    second commit

--- script.js
+++ script.js
@@ -3,8 +3,27 @@ document.getElementById('calculate-btn').addEventListener('click',function(){
     const rentInput = document.getElementById('rent-input').value
     const clothesInput = document.getElementById('clothes-input').value
 
-    const totalExpenses = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothesInput)
+    const allExpenses = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothesInput)
     
+    
+    // totalExpenses
+    const totalExpenses = document.getElementById('total-expences')
+    const totalExpensesText = totalExpenses.innerText
+    totalExpenses.innerText = allExpenses
+ 
+  
+//    income
+    const myIncome = document.getElementById('income-input').value
+    const myIncomeFloat = parseFloat(myIncome)
+    const totalIncome = myIncomeFloat
+   
    
 
+   // Balance
+
+   const balance = document.getElementById('balance')
+   const balancetext = balance.innerText
+   balance.innerText = totalIncome - parseFloat( totalExpenses.innerText)
+   
+ 
 })
