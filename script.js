bec4faeb8ccea236675c592b6cbb9921a5c6a023
TaHasib981 <tahaisb981@gmail.com>
2022-02-17 15:51:21 +0600

    third commit

--- script.js
+++ script.js
@@ -27,3 +27,32 @@ document.getElementById('calculate-btn').addEventListener('click',function(){
    
  
 })
+
+// save section
+
+document.getElementById('save-button').addEventListener('click',function(){
+   
+    const saveInput = document.getElementById('save-input').value 
+    const saveInputFloat = parseFloat(saveInput)
+ 
+ 
+     const myIncome = document.getElementById('income-input').value
+     const myIncomeFloat = parseFloat(myIncome)
+     const totalIncome = myIncomeFloat
+ 
+     const savingAmmount = document.getElementById('saving-ammout')
+     const savingAmmountText = savingAmmount.innerText
+     const savingAmmountFloat = parseFloat(savingAmmountText)
+     savingAmmount.innerText = (totalIncome/100)*saveInputFloat
+ //  console.log(savingAmmount.innerText)
+     //remaining balance
+    const previousBalance = document.getElementById('balance')
+    const previousBalanceText =previousBalance.innerText
+    const previousBalanceFloat = parseFloat(previousBalanceText)
+ //   console.log(previousBalanceFloat) 
+ 
+   const remainingAmmount = document.getElementById('remaining-balance')
+   const remainingAmmountText = remainingAmmount.innerText
+   const finalAmmount = previousBalanceFloat - parseFloat( savingAmmount.innerText)
+   remainingAmmount.innerText = finalAmmount
+ })
